e2d6fcfcbb8b7b695d79bbe7484cfb2b469a4d25
HoldenCaulfieldICL <ad6813@imperial.ac.uk>
2014-01-03 17:08:45 +0000

    bloarb

--- tut3.pl
+++ tut3.pl
@@ -1,6 +1,12 @@
 
 partition([], _, [], []).
 
+partition([A,B], _, [A], [B]):-
+	A < B.
+
+partition([A,B], _, [B], [A]):-
+	\+ A < B.
+
 partition([H|T], P, Parti1, Parti2):-
 	H < P,!,
 	partition(T, P, RestParti1, Parti2),
@@ -33,17 +39,24 @@ quicksort([H|[]], [H]).
 % 	 append(Sorted1, Sorted2, Sorted)
 % 	 ).
 
+% quicksort([H|T], Sorted):-
+% 	P is H+1,
+% 	partition([H|T], P, Parti1, Parti2),
+% 	(
+% 	 Parti1=[],
+% 	 quicksort(Parti2, Sorted)
+% 	;
+% 	 Parti2=[],
+% 	 quicksort(Parti1, Sorted)
+% 	;
+% 	 quicksort(Parti1, Sorted1),
+% 	 quicksort(Parti2, Sorted2),
+% 	 append(Sorted1, Sorted2, Sorted)
+% 	).
+
+
 quicksort([H|T], Sorted):-
-	P is H+1,
-	partition([H|T], P, Parti1, Parti2),
-	(
-	 Parti1=[],
-	 quicksort(Parti2, Sorted)
-	;
-	 Parti2=[],
-	 quicksort(Parti1, Sorted)
-	;
+	 partition([H|T], H-1, Parti1, Parti2),
 	 quicksort(Parti1, Sorted1),
 	 quicksort(Parti2, Sorted2),
-	 append(Sorted1, Sorted2, Sorted)
-	).
+	 append(Sorted1, Sorted2, Sorted).
