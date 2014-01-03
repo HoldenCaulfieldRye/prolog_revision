b016ddf70497e44159240f1a05565c8286e31cbe
HoldenCaulfieldICL <ad6813@imperial.ac.uk>
2014-01-03 14:23:11 +0000

    tut2

--- tut2.pl
+++ tut2.pl
@@ -1,16 +1,47 @@
-input1(G,I).
+andgate(And).
 
-input2(G,I).
+orgate(Or).
 
-output(G,O).
-
-andgate(G).
-
-orgate(G).
-
-xorgate(G).
+xorgate(Xor).
 
 
+% and gate
 output(G,1):-
 	andgate(G),
+	input1(G,1),
+	input2(G,1).
+
+output(G,0):-
+	andgate(G),
+	\+ output(G,1).
+
+
+% or gate
+output(G,0):-
+	orgate(G),
+	input1(G,0),
+	input2(G,0).
+
+output(G,1):-
+	orgate(G),
+	\+ output(G,0).
+
+
+
+% xor gate
+output(G,1):-
+	orgate(G),
+	(
+	 input1(G,1),
+	 input2(G,0)
+	;
+	 input1(G,0),
+	 input2(G,1)
+	).
+
+output(G,0):-
+	orgate(G),
+	\+ output(G,1).
+
+	 
 
