b43cc4e28a534801f09e02b5a7967d5946ce9e75
HoldenCaulfieldICL <ad6813@imperial.ac.uk>
2014-01-03 19:50:26 +0000

    followedBy

--- tut3/rev.pl
+++ tut3/rev.pl
@@ -4,22 +4,3 @@ rev([H|T], Rev):-
 	rev(T, TailRev),
 	append(TailRev, [H], Rev).
 
-
-
-followedBy([], [X], [X|_]).
-
-followedBy([], [H2|T2], [H3|T3]):-
-	H2=H3,
-	followedBy([], T2, T3).
-	
-followedBy([H1|T1], [H2|T2], [H3|T3]):-
-	\+ H1 = H3,!,
-	followedBy([H1|T1], [H2|T2], T3).
-
-followedBy([H1|T1], [H2|T2], [H3|T3]):-
-	H1 = H3,!,
-	(
-	 followedBy([H1|T1], [H2|T2], T3)
-	;
-	 followedBy(T1, [H2|T2], T3)
-	).
