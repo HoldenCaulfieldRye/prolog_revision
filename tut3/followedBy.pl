49466221fc93a12b4fa2ae81797e6531033134eb
HoldenCaulfieldICL <ad6813@imperial.ac.uk>
2014-01-03 22:54:55 +0000

    followedBy corrected successfully

--- tut3/followedBy.pl
+++ tut3/followedBy.pl
@@ -1,17 +1,8 @@
-followedBy([], [X], [X|_]).
 
-followedBy([], [H2|T2], [H3|T3]):-
-	H2=H3,
-	followedBy([], T2, T3).
-	
-followedBy([H1|T1], [H2|T2], [H3|T3]):-
-	\+ H1 = H3,
-	followedBy([H1|T1], [H2|T2], T3).
+followedBy(A, B, [H|T]):-
+	A = H,
+	T = [H2|_],
+	B = H2.
 
-followedBy([H1|T1], [H2|T2], [H3|T3]):-
-	H1 = H3,
-	(
-	 followedBy([H1|T1], [H2|T2], T3)
-	;
-	 followedBy(T1, [H2|T2], T3)
-	).
+followedBy(A, B, [_|T]):-
+	followedBy(A, B, T).
--- tut3/followedBy_list.pl
+++ tut3/followedBy_list.pl
@@ -1,27 +1,17 @@
-% followedBy([], [X], [X|_]).
+followedBy([], [X], [X|_]).
 
-% followedBy([], [H2|T2], [H3|T3]):-
-% 	H2=H3,
-% 	followedBy([], T2, T3).
+followedBy([], [H2|T2], [H3|T3]):-
+	H2=H3,
+	followedBy([], T2, T3).
 	
-% followedBy([H1|T1], [H2|T2], [H3|T3]):-
-% 	\+ H1 = H3,
-% 	followedBy([H1|T1], [H2|T2], T3).
+followedBy([H1|T1], [H2|T2], [H3|T3]):-
+	\+ H1 = H3,
+	followedBy([H1|T1], [H2|T2], T3).
 
-% followedBy([H1|T1], [H2|T2], [H3|T3]):-
-% 	H1 = H3,
-% 	(
-% 	 followedBy([H1|T1], [H2|T2], T3)
-% 	;
-% 	 followedBy(T1, [H2|T2], T3)
-% 	).
-
-
-followedBy(A, B, [H|T]):-
-	A = H,
+followedBy([H1|T1], [H2|T2], [H3|T3]):-
+	H1 = H3,
 	(
-	 T = [H2|T2],
-	 B = H2
+	 followedBy([H1|T1], [H2|T2], T3)
 	;
-	 followedBy(A, B, T)
+	 followedBy(T1, [H2|T2], T3)
 	).
