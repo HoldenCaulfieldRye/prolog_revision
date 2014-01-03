2e434f3556244f662383f8432ddcf34d323be1f8
HoldenCaulfieldICL <ad6813@imperial.ac.uk>
2014-01-03 16:29:23 +0000

    this might just be it

--- tut3.pl
+++ tut3.pl
@@ -16,10 +16,30 @@ partition([H|T], P, Parti1, Parti2):-
 quicksort([], []).
 
 % need to avoid infinite loop (you can visualise this)
-quicksort([H], [H]).
+quicksort([H|[]], [H]).
+
 
 quicksort([H|T], Sorted):-
-	partition([H|T], H, Parti1, Parti2),!,
+	partition([H|T], H, Parti1, Parti2),
+	Parti1=[],
+	quicksort(Parti2, Sorted).
+
+	
 	quicksort(Parti1, Sorted1),
 	quicksort(Parti2, Sorted2),
 	append(Sorted1, Sorted2, Sorted).
+
+
+quicksort([H|T], Sorted):-
+	partition([H|T], H, Parti1, Parti2),
+	(
+	 Parti1=[],
+	 quicksort(Parti2, Sorted)
+	;
+	 Parti2=[],
+	 quicksort(Parti1, Sorted)
+	;
+	quicksort(Parti1, Sorted1),
+	quicksort(Parti2, Sorted2),
+	append(Sorted1, Sorted2, Sorted)
+	).
