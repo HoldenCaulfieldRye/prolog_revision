7e74ea20f4fd19ca858cba2eb2f55cf46e91cdf3
alex <adalyac@gmail.com>
2014-01-06 12:48:19 +0000

    last.pl

--- tut3/followedBy_list.pl
+++ tut3/followedBy_list.pl
@@ -15,3 +15,14 @@ followedBy([H1|T1], [H2|T2], [H3|T3]):-
 	;
 	 followedBy(T1, [H2|T2], T3)
 	).
+
+
+contained(H, [H|T]).
+
+contained([H1|T1], [H|T]):-
+	H1 = H,
+	contained(T1, T).
+
+contained(L, [H|T]):-
+	contained(L, T). 
+
