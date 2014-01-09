f5e5903b9d47b134db0a18ba1d0e5fbac0648da7
HoldenCaulfieldICL <adalyac@gmail.com>
2014-01-09 14:30:20 +0000

    p4_p8 done

--- 99prologQuestions/p4_p8.pl
+++ 99prologQuestions/p4_p8.pl
@@ -25,35 +25,75 @@ palindrome(L):-
 
 %%%%%%%% P7 %%%%%%%%%%%%%%%
 
+% YOU HAD:
+
+% is_list(L):-
+%         L = [_|_].
+
+% my_flatten([X], [X]):-
+%         \+ is_list(X).
+
+% my_flatten([H|T], [H|T]):-
+%         \+ is_list(H),
+%         my_flatten(T,T).
+
+% my_flatten([Y], L):-
+%         is_list(Y),
+%         my_flatten(Y, L).
+
+% my_flatten([H|T], [H1|T1]):-
+%         my_flatten(H, [H2|T2]),
+%         H1 = H2,
+%         my_flatten(T, L),
+%         append([H2|T2], L, [H1|T1]).
+
+
 is_list(L):-
 	L = [_|_].
 
+% another base case (why needed?)
+my_flatten([],[]).
+
+
 % base case
-my_flatten([X], [X]):-
+% this is actually a big deal! you had my_flatten([X],[X]) instead
+% so it doesn't just work on lists, it works on an element too, and
+% it doesn't flatten it, or leave it as it is, but deepens it!
+% why? read comments on next predicate
+my_flatten(X, [X]):-
 	\+ is_list(X).
 
-% case where list is already flat
-my_flatten([H|T], [H|T]):-
-	\+ is_list(H),
-	my_flatten(T,T).
 
-% case where list is one single clogged up element
-my_flatten([Y], L):-
-	is_list(Y),
-	my_flatten(Y, L).
-
-
-my_flatten([H|T], [H1|T1]):-
-	my_flatten(H, [H2|T2]),
-	H1 = H2,
-	my_flatten(T, L),
-	append([H2|T2], L, [H1|T1]).
-	
-	
+% recursive
+% usual recursive prolog predicate structure is treat the head, then
+% recursively call the tail, and append the two.
+% but this predicate treats lists, and the head is not a list. so you
+% need to bend the rules and assign a list to a treated element.
+% incidentally, this does the opposite of flattening for that special
+% case. but it works!
+my_flatten([X|Xs],Zs):-
+	my_flatten(X,Y),
+	my_flatten(Xs,Ys),
+	append(Y,Ys,Zs).
 
 
-% my_flatten(L, [X]):-
-% 	member(X, L1),
-	
 
-	
+%%%%%%%% P8 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% compress([],[]).
+
+compress([X],[X]):-
+	\+ is_list(X).
+
+compress([H|T],L):-
+	compress(T, L1),
+	[H1|_] = T,
+	(
+	 \+ H1 = H,
+	 L = [H|L1]
+	;
+	 H1 = H,
+	 L = L1
+	).
+
+
